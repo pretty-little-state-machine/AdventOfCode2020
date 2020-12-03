b96ebc0c04edfbfa98599d8db244a0a30a079b06
Zoey Fahner <zoeyfahner@gmail.com>
2020-12-03 08:43:35 -0500

    COBOL lol

---
+++ day_03.cob
@@ -0,0 +1,43 @@
+      * Sample COBOL program
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. advent-of-code-day-03.
+       ENVIRONMENT DIVISION.
+       INPUT-OUTPUT SECTION.
+       FILE-CONTROL.
+           SELECT MAP ASSIGN TO './files/day_3_input.txt'
+           ORGANIZATION IS LINE SEQUENTIAL.
+
+       DATA DIVISION.
+       FILE SECTION.
+       FD MAP
+           LABEL RECORDS ARE OMITTED.
+       01 INPUTRECORD PIC X(31).
+
+       WORKING-STORAGE SECTION. 
+       01  MAP-TOPO.
+           05 FIELD PIC A(31).
+       01  MOVEMENT-SELECTOR PIC 9(02) VALUE 0.
+       01  COORD-X PIC 9(02) VALUE 1.
+       01  ITEM PIC X(01).
+       01  TREES PIC 9(03) VALUE 0.
+       01  FILE-STATUS PIC 9 VALUE 0.
+       01  FIRST-LINE PIC 9 VALUE 1.
+
+       PROCEDURE DIVISION.
+           OPEN INPUT MAP.
+           PERFORM UNTIL FILE-STATUS = 1
+              READ MAP INTO MAP-TOPO
+                  AT END MOVE 1 TO FILE-STATUS
+                  NOT AT END MOVE MAP-TOPO(COORD-X:1) TO ITEM
+                     IF ITEM IS EQUAL TO "#"
+                          ADD 1 TO TREES
+                     END-IF
+                     ADD 3 TO COORD-X
+                     IF COORD-X IS GREATER THAN 31
+                          SUBTRACT 31 FROM COORD-X
+                     END-IF
+              END-READ
+           END-PERFORM.
+           DISPLAY TREES
+           CLOSE MAP.
+           STOP RUN.
