d3f4d5cc32517cb405497b6e25160f5e7b972574
Zoey Fahner <zoeyfahner@gmail.com>
2020-12-07 14:06:12 -0500

    Day 07

---
+++ day_07.exs
@@ -0,0 +1,99 @@
+defmodule Advent do
+  def part_1(input) do
+    bag_rules = parse_input(input)
+    [graph, verts] = run_digraph(bag_rules)
+    count_num_paths_to_color(bag_rules, graph, verts, "shiny gold")
+  end
+
+  def part_2(input) do
+    bag_rules = parse_input(input)
+    sum_child_bags(1, bag_rules, "shiny gold") - 1
+  end
+
+  defp parse_input(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(fn x -> parse_line(x) end)
+  end
+
+  defp parse_line(line) do
+    color =
+      line
+      |> String.split(" bags")
+      |> List.first()
+
+    holds =
+      line
+      |> String.split("contain ")
+      |> List.last()
+      |> String.split(", ")
+      |> extract_may_hold()
+
+    %{color: color, holds: holds}
+  end
+
+  defp extract_may_hold(["no other bags."]) do
+    []
+  end
+
+  defp extract_may_hold(list) do
+    list
+    |> Enum.map(fn x ->
+      s = String.split(x)
+      %{count: String.to_integer(List.first(s)), color: Enum.at(s, 1) <> " " <> Enum.at(s, 2)}
+    end)
+  end
+
+  defp run_digraph(bag_rules) do
+    g = :digraph.new()
+    # Make a map of verts for fast-lookup later
+    verts =
+      bag_rules
+      |> Enum.reduce(%{}, fn v, acc ->
+        Map.put(acc, v.color, :digraph.add_vertex(g, v.color))
+      end)
+
+    # Edges
+    bag_rules
+    |> Enum.each(fn x ->
+      x.holds
+      |> Enum.each(fn h ->
+        :digraph.add_edge(g, verts[x.color], verts[h.color], h.count)
+      end)
+    end)
+
+    [g, verts]
+  end
+
+  defp count_num_paths_to_color(bag_rules, graph, verts, color) do
+    bag_rules
+    |> Enum.reduce(0, fn x, acc ->
+      if has_path?(graph, verts[x.color], color) do
+        acc + 1
+      else
+        acc
+      end
+    end)
+  end
+
+  defp sum_child_bags(mul, bag_rules, bag_color) do
+    seek_bag = bag_rules |> Enum.find(fn x -> x.color == bag_color end)
+
+    sum =
+      seek_bag.holds
+      |> Enum.reduce(1, fn x, a -> a + sum_child_bags(x.count, bag_rules, x.color) end)
+
+    mul * sum
+  end
+
+  defp has_path?(graph, v1, v2) do
+    false != :digraph.get_short_path(graph, v1, v2)
+  end
+end
+
+{:ok, input} = File.read("./files/day_07_input.txt")
+part_1 = Advent.part_1(input)
+IO.puts("Part 1: #{part_1}")
+
+part_2 = Advent.part_2(input)
+IO.puts("Part 1: #{part_2}")
