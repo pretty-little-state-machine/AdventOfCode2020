e6ed8e5b5f1cd0ea30917f88e1fa3cb4273bc448
Zoey <zoey.fahner@gmail.com>
2020-12-24 11:10:04 -0500

    Day 24! Part 2, the GC crusher

--- day_24.exs
+++ day_24.exs
@@ -7,6 +7,8 @@ defmodule Hexa do
   Read this: https://www.redblobgames.com/grids/hexagons/
   """
   def new(), do: %{x: 0, y: 0, z: 0, color: :white}
+  def new(x, y, z), do: %{x: x, y: y, z: z, color: :white}
+
   def move(hex, :ne), do: move(hex, 1, 0, -1)
   def move(hex, :e), do: move(hex, 1, -1, 0)
   def move(hex, :se), do: move(hex, 0, -1, 1)
@@ -32,6 +34,60 @@ defmodule Advent do
     |> get_initial_tilestate()
     |> count_tiles()
     |> Map.get(:black)
+  end
+
+  def part_2() do
+    tiles = parse_file() |> get_initial_tilestate()
+    # Garbage collector goes Brrrrrrrrrrrr.... This is horrific.
+    Enum.reduce(1..100, tiles, fn i, acc ->
+      res = run_rules(acc)
+      count = count_tiles(res) |> Map.get(:black)
+      IO.puts("Run #{i}:  #{count}")
+      res
+    end)
+  end
+
+  def run_rules(tiles) do
+    # Expand the space first to all potential neighbors
+    tiles =
+      Enum.reduce(tiles, tiles, fn {_key, tile}, acc ->
+        neighbors = get_neighbors(tiles, tile)
+        Map.merge(neighbors, acc)
+      end)
+
+    # Now run the ruleset on the expanded space
+    Enum.reduce(tiles, tiles, fn {_key, tile}, acc ->
+      neighbors = get_neighbors(tiles, tile)
+      new_tile = run_rule(tile, neighbors |> count_tiles())
+      # Do not replace any existing tiles, but add neighbors and update the tile
+      Map.merge(neighbors, acc) |> Map.put(Hexa.to_key(new_tile), new_tile)
+    end)
+  end
+
+  def run_rule(tile, counts) when tile.color == :black and counts.black == 0,
+    do: Hexa.flip(tile)
+
+  def run_rule(tile, counts) when tile.color == :black and counts.black > 2,
+    do: Hexa.flip(tile)
+
+  def run_rule(tile, counts) when tile.color == :white and counts.black == 2,
+    do: Hexa.flip(tile)
+
+  def run_rule(tile, _), do: tile
+
+  def get_neighbors(tiles, tile) do
+    [:ne, :e, :se, :sw, :w, :nw]
+    |> Enum.reduce(%{}, fn dir, acc ->
+      n = get_neighbor(tiles, tile, dir)
+      Map.merge(acc, n)
+    end)
+  end
+
+  def get_neighbor(tiles, tile, direction) do
+    cur = tile |> Hexa.move(direction)
+    # Builds a new tile if the neighbor doesn't exist yet.
+    n = Map.get(tiles, Hexa.to_key(cur), Hexa.new(cur.x, cur.y, cur.z))
+    Map.put(%{}, Hexa.to_key(n), Map.get(tiles, Hexa.to_key(n), Hexa.new(n.x, n.y, n.z)))
   end
 
   def get_initial_tilestate(tiles) do
@@ -68,3 +124,4 @@ defmodule Advent do
 end
 
 IO.puts("Part 1: #{Advent.part_1()} black tiles")
+Advent.part_2()
