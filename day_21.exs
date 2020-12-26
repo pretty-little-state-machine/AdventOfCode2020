648e0ab201dfaf13e7cbf43f96f16b19bc5226fe
Zoey <zoey.fahner@gmail.com>
2020-12-25 21:58:00 -0500

    Day 21... um, it's working? Horrific.

--- day_21.exs
+++ day_21.exs
@@ -3,53 +3,80 @@ alias ZoeysHelpers, as: Z
 
 defmodule Advent do
   def part_1() do
-    parse("files/day_21_input.txt")
+    parse()
+
+    forbidden =
+      allergen_ingredients()
+      |> Map.values()
+      |> Enum.reduce(&MapSet.union/2)
+
+    parse()
+    |> Stream.map(&elem(&1, 0))
+    |> Stream.concat()
+    |> Enum.count(&(&1 not in forbidden))
   end
 
-  defp parse(file) do
-    foods =
-      Z.file_to_list(file)
-      |> Enum.map(fn line ->
-        [i, a] = String.split(line, "(contains ")
-        ingredients = String.split(i, " ") |> Enum.reject(fn x -> x == "" end)
-        allergens = String.replace(a, ")", "") |> String.split(",") |> Enum.map(&String.trim(&1))
+  def part_2() do
+    input = allergen_ingredients()
 
-        %{ingredients: ingredients, allergens: allergens}
-      end)
+    Enum.reduce_while(1..1_000, %{input: input, found: %{}}, fn x, outer_acc ->
+      result =
+        Enum.reduce(outer_acc.input, %{a: outer_acc.input, found: %{}}, fn {key, value}, acc ->
+          if MapSet.size(value) == 1 do
+            # Clean up map
+            new_set =
+              Map.drop(acc.a, [key])
+              |> Enum.reduce(%{}, fn {key, new_set}, acc ->
+                Map.put(acc, key, MapSet.difference(new_set, value))
+              end)
 
-    foods |> make_allergen_potentials()
-
-    # foods |> ingredient_frequencies()
-  end
-
-  @doc """
-  Makes a list of potential allergens for each item.
-  """
-  def make_allergen_potentials(foods) do
-    Enum.reduce(foods, [], fn f, f_acc ->
-      perms =
-        Z.permutations([f.ingredients, f.allergens])
-        |> Enum.reduce(%{}, fn p, acc ->
-          [ingredient, allergen] = p
-          value = Map.get(acc, ingredient, []) ++ [allergen]
-          Map.put(acc, ingredient, value)
+            Map.put(acc, :a, new_set)
+            |> Map.put(:found, Map.put(acc.found, value, key))
+          else
+            acc
+          end
         end)
 
-      f_acc ++ [perms]
+      outer_acc =
+        Map.put(outer_acc, :found, Map.merge(outer_acc.found, Map.get(result, :found)))
+        |> Map.put(:input, Map.get(result, :a))
+
+      if %{} == result.a do
+        {:halt, outer_acc.found}
+      else
+        {:cont, outer_acc}
+      end
     end)
+    |> Enum.sort_by(fn {_, v} -> v end)
+    |> Enum.map(fn {mapset, _} -> Enum.at(mapset, 0) end)
+    |> Enum.join(",")
     |> Z.debug()
   end
 
-  def ingredient_frequencies(foods) do
-    ingredients =
-      Enum.reduce(foods, [], fn x, acc ->
-        acc ++ x.ingredients
-      end)
-      |> Enum.frequencies()
-      |> Z.debug()
+  defp parse() do
+    Z.file_to_list("files/day_21_input.txt")
+    |> Enum.map(fn line ->
+      [i, a] = String.split(line, "(contains ")
+      ingredients = String.split(i, " ") |> Enum.reject(fn x -> x == "" end)
+      allergens = String.replace(a, ")", "") |> String.split(",") |> Enum.map(&String.trim(&1))
+
+      {ingredients, allergens}
+    end)
+  end
+
+  def allergen_ingredients do
+    parse()
+    |> Enum.map(&candidates/1)
+    |> Enum.reduce(&merge/2)
+  end
+
+  def merge(c1, c2), do: Map.merge(c1, c2, fn _, s1, s2 -> MapSet.intersection(s1, s2) end)
+
+  def candidates({ingredients, allergens}) do
+    for allergen <- allergens, into: %{}, do: {allergen, MapSet.new(ingredients)}
   end
 end
 
-Advent.part_1()
+Advent.part_2()
 # IO.puts("Part 1: #{Advent.part_1()}")
 # Advent.part_2()
